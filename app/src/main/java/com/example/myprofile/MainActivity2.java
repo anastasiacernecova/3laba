dcfcc225166470c4751b1f79c203c5036d675dd7
anastasiacernecova <ChernezovaAsja666@yandex.ru>
2024-11-28 10:52:24 +0300

    3 лаба изменено

--- app/src/main/java/com/example/myprofile/MainActivity2.java
+++ app/src/main/java/com/example/myprofile/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.myprofile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void razrabotchik2(View view) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
